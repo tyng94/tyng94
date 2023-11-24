b59abb211e1e038e804e75ccae5e6a0c08592173
Tze Yang Ng <tzeyang@dsaid.gov.sg>
2023-11-25 00:22:32 +0800

    terraform ci

--- _infrastructure/terraform.tf
+++ _infrastructure/terraform.tf
@@ -10,3 +10,39 @@ resource "aws_s3_bucket_public_access_block" "terraform" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = data.aws_s3_bucket.terraform.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "DenyUnEncryptedObjectUploads"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:PutObject"
+        Resource  = "${data.aws_s3_bucket.terraform.arn}/*"
+        Condition = {
+          StringNotEquals = {
+            "s3:x-amz-server-side-encryption" : "aws:kms"
+          }
+        }
+      },
+      {
+        Sid       = "DenyInsecureConnections"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          "${data.aws_s3_bucket.terraform.arn}",
+          "${data.aws_s3_bucket.terraform.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" : false
+          }
+        }
+      }
+    ]
+  })
+}
