4101c80a604a2475e22f45eda58cbaab707af01c
Tze Yang Ng <tzeyang@dsaid.gov.sg>
2023-11-25 02:08:05 +0800

    attach straight to user

--- _infrastructure/iam.tf
+++ _infrastructure/iam.tf
@@ -6,35 +6,15 @@ resource "aws_iam_access_key" "continuous_integration" {
   user = aws_iam_user.continuous_integration.name
 }
 
-resource "aws_iam_role" "terraformer" {
-  name = "terraformer"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          AWS = aws_iam_user.continuous_integration.arn
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "terraformer_policy" {
-  name = "terraformer"
-  role = aws_iam_role.terraformer.name
-
+resource "aws_iam_policy" "terraform_base_policy" {
+  name        = "terraform-base-policy"
+  description = "Policy for terraformer to perform base operations"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = [
-          "ec2:*",
           "iam:*",
-          "kms:*",
-          "logs:*",
           "rds:*",
           "s3:*",
         ]
@@ -43,4 +23,9 @@ resource "aws_iam_role_policy" "terraformer_policy" {
       }
     ]
   })
+}
+
+resource "aws_iam_user_policy_attachment" "continuous_integration_policies" {
+  user       = aws_iam_user.continuous_integration.name
+  policy_arn = aws_iam_policy.terraform_base_policy.arn
 }
